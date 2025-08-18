eb273e8320881615a140c684824f311590551cce
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2025-08-18 16:56:28 +0200

    add ALT+Enter to trigger code actions
    some JetBrains nostalgia for now

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- normal mode: Alt+Enter
+vim.keymap.set("n", "<M-CR>", vim.lsp.buf.code_action, { desc = "Code Action (Alt+Enter)" })
+
+-- visual mode: Alt+Enter
+vim.keymap.set("v", "<M-CR>", vim.lsp.buf.code_action, { desc = "Code Action (Alt+Enter)" })
